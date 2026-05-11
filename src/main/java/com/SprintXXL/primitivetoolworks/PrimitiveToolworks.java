507b918bb3fa9a5c96debfc0c7b8e18e858d8327
SprintXXL <mspmain@pm.me>
2026-05-11 02:13:33 +0200

    Primitive Toolworks Base Mod

---
+++ src/main/java/com/SprintXXL/primitivetoolworks/PrimitiveToolworks.java
@@ -0,0 +1,27 @@
+package com.SprintXXL.primitivetoolworks;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = PrimitiveToolworks.MODID, name = PrimitiveToolworks.NAME, version = PrimitiveToolworks.VERSION)
+public class PrimitiveToolworks
+{
+    public static final String MODID = "primitivetoolworks";
+    public static final String NAME = "Primitive Toolworks";
+    public static final String VERSION = "0.4.0";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+    }
+}
